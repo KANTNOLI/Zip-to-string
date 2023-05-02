c2663059f9d43275e17240450501709438b1882d
KANTNOLI <zahargysev02@gmail.com>
2023-05-02 11:14:57 +0300

    Optimization start

--- Zip to string/logic.cpp
+++ Zip to string/logic.cpp
@@ -6,37 +6,32 @@ string converting_to_zip(string convert, int sizex, int sizey) {
 	char char2 = convert[count2];
 	string msg = "";
 	int work_size = sizex * sizey;
-	
 
 	for (int i = 0; i < work_size; i++)
 	{
 		char char1 =  convert[i];
-		char2 =  convert[i + 1];
-
+		char2 = convert[i + 1];
 		if (char1 == char2)
 		{
 			count++;
 		}
 		else {
-			msg += to_string(count) + char1;
+			msg += (count == 1 ? char1 : count + char1);
 			count = 1;
 		}
-		
+
 		if (count2 == sizex)
 		{
-			if (sizex - count < sizex && count - 1 > 0)
+			if (sizex - count < sizex && count > 1)
 			{
-				msg += to_string(count - 1) + char1;
+				msg += (count == 1  ? char1 : count + char1);
 			}
 			msg += "\n";
 			count = 1;
 			count2 = 0;
 		}
-
 		count2++;
 	}
-	
-
 
 	return msg;
 }
